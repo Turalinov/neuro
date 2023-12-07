021de7277484f04208163d9ee8722c65ccda130d
Ruslan Turalinov <rusltur_24@mail.ru>
2023-12-07 23:34:35 +0600

    added neuro

--- docs/upload.php
+++ docs/upload.php
@@ -3,42 +3,6 @@
 print_r($_FILES);
 $uploadDir =  __DIR__ . '/vendor/uploads/';
 
-function generateNewFileName($originalFileName) {
-    // Получение текущей даты и времени в формате YYYYMMDDhhnmmss
-    $datePart = date('YmdHis');
-
-    // Транслитерация символов с кириллицы на латиницу
-    $transliteratedFileName = transliterate($originalFileName);
-
-    // Замена пробелов на нижние подчеркивания
-    $sanitizedFileName = preg_replace('/\s+/', '_', $transliteratedFileName);
-
-    // Сборка нового имени файла
-    $newFileName = $datePart . '_' . $sanitizedFileName;
-
-    return $newFileName;
-}
-
-function transliterate($text) {
-    $transliterationTable = array(
-        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
-        'е' => 'e', 'ё' => 'e', 'ж' => 'zh', 'з' => 'z', 'и' => 'i',
-        'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
-        'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
-        'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
-        'ш' => 'sh', 'щ' => 'sch', 'ь' => '', 'ы' => 'y', 'ъ' => '',
-        'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
-        'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D',
-        'Е' => 'E', 'Ё' => 'E', 'Ж' => 'Zh', 'З' => 'Z', 'И' => 'I',
-        'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N',
-        'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T',
-        'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C', 'Ч' => 'Ch',
-        'Ш' => 'Sh', 'Щ' => 'Sch', 'Ь' => '', 'Ы' => 'Y', 'Ъ' => '',
-        'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya',
-    );
-
-    return strtr($text, $transliterationTable);
-}
 
 if (isset($_FILES)) {
 
